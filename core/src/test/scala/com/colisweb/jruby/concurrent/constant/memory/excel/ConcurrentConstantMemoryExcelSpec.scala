30ec9790546fb0a6f242e99a4a59f7c7074df2b7
Jules Ivanic <jules.ivanic@gmail.com>
2026-04-13 20:30:01 +1000

    Replace Monix with ZIO, ScalaTest with zio-test
    - Replace monix-eval/monix-execution with zio 2.1.25
    - Replace cats.effect.Resource with ZIO.acquireRelease + Scope
    - Replace monix Atomic with java.util.concurrent.atomic.AtomicReference
    - Replace Task(...) with ZIO.attempt(...)
    - Replace Task.parSequenceUnordered with ZIO.collectAllParDiscard
    - Replace .runSyncUnsafe() with Unsafe.unsafe { Runtime.default.unsafe.run(...) }
    - Replace ScalaTest/ScalaCheck with zio-test
    - Rewrite tests using ZIOSpecDefault and assertTrue

--- core/src/test/scala/com/colisweb/jruby/concurrent/constant/memory/excel/ConcurrentConstantMemoryExcelSpec.scala
+++ core/src/test/scala/com/colisweb/jruby/concurrent/constant/memory/excel/ConcurrentConstantMemoryExcelSpec.scala
@@ -1,21 +1,14 @@
 package com.colisweb.jruby.concurrent.constant.memory.excel
 
-import monix.execution.atomic.Atomic
-import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should.Matchers
+import zio.test.*
 
 import java.io.File
 import java.nio.file.Files
 import java.util.Date
-import scala.annotation.nowarn
+import java.util.concurrent.atomic.AtomicReference
 import scala.language.implicitConversions
 
-@nowarn("msg=unused value")
-class ConcurrentConstantMemoryExcelSpec extends AnyFlatSpec with Matchers {
-
-  "true" should "be true" in {
-    true shouldBe true
-  }
+object ConcurrentConstantMemoryExcelSpec extends ZIOSpecDefault {
 
   import ConcurrentConstantMemoryExcel.*
 
@@ -27,99 +20,107 @@ class ConcurrentConstantMemoryExcelSpec extends AnyFlatSpec with Matchers {
   given Conversion[Double, Cell] = value => numericCell(value)
   given Conversion[Int, Cell]    = value => numericCell(value.toDouble)
 
-  def newCMSPlz: Atomic[ConcurrentConstantMemoryState] = newWorkbookState(sheet_name, headers)
-  def row(cells: Cell*): Array[Cell]                   = cells.toArray
+  def newCMSPlz: AtomicReference[ConcurrentConstantMemoryState] = newWorkbookState(sheet_name, headers)
+  def row(cells: Cell*): Array[Cell]                            = cells.toArray
 
-  "ConcurrentConstantMemoryExcel#addRows" should "write a tmp CSV file" in {
-    val cms = newCMSPlz
+  override def spec = suite("ConcurrentConstantMemoryExcel")(
+    test("true is true") {
+      assertTrue(true)
+    },
+    test("addRows writes a tmp CSV file") {
+      val cms = newCMSPlz
 
-    val data: Array[Row] = Array(
-      row("a0", "b0", 0),
-      row("a1", "b1", 1),
-      row("a2", "b2", 2),
-    )
+      val data: Array[Row] = Array(
+        row("a0", "b0", 0),
+        row("a1", "b1", 1),
+        row("a2", "b2", 2),
+      )
 
-    addRows(cms, data, 0)
+      addRows(cms, data, 0)
 
-    cms.get().pages should not be empty
-  }
+      assertTrue(cms.get().pages.nonEmpty)
+    },
+    test("writeFile writes the xlsx file") {
+      val cms = newCMSPlz
 
-  "ConcurrentConstantMemoryExcel#writeFile" should "write the xlsx file" in {
-    val cms = newCMSPlz
+      val data0: Array[Row] = Array(
+        row("a0", "b0", 0),
+        row("a1", "b1", 1),
+        row("a2", "b2", 2),
+      )
 
-    val data0: Array[Row] = Array(
-      row("a0", "b0", 0),
-      row("a1", "b1", 1),
-      row("a2", "b2", 2),
-    )
+      val data1: Array[Row] = Array(
+        row("a01", "b01", 10),
+        row("a11", "b11", 11),
+        row("a21", "b21", 12),
+      )
 
-    val data1: Array[Row] = Array(
-      row("a01", "b01", 10),
-      row("a11", "b11", 11),
-      row("a21", "b21", 12),
-    )
+      val data2: Array[Row] = Array(
+        row("a02", "", 20),
+        row("a12", "", 21),
+        row("a22", "", 22),
+      )
 
-    val data2: Array[Row] = Array(
-      row("a02", "", 20),
-      row("a12", "", 21),
-      row("a22", "", 22),
-    )
+      addRows(cms, data2, 10)
+      addRows(cms, data1, 20)
+      addRows(cms, data0, 15)
 
-    addRows(cms, data2, 10)
-    addRows(cms, data1, 20)
-    addRows(cms, data0, 15)
+      val fileName = s"target/fileName-${new Date()}.xlsx"
 
-    val fileName = s"target/fileName-${new Date()}.xlsx"
+      writeFile(cms, fileName)
 
-    writeFile(cms, fileName)
+      assertTrue(
+        new File(fileName).exists(),
+        cms.get().pages.nonEmpty,
+        !cms.get().pages.forall(page => Files.exists(page.path)), // clean the tmp CSV files automatically.
+      )
+    },
+    test("writeFile does not change the font size if the text is long") {
+      val cms = newCMSPlz
 
-    new File(fileName).exists() shouldBe true
-    cms.get().pages should not be empty
-    cms.get().pages.forall(page => Files.exists(page.path)) shouldBe false // clean the tmp CSV files automatically.
-  }
+      val data0: Array[Row] = Array(
+        row(
+          """mqldnqs:;dn:q;nf;dqskdqshlfqldmqfnzlfnas:d,asqnf;q:dsd;nq:s;dnqs:;nd;qns:=:dkg;krgljz,q:snd:,;qs,:f:;qsfcsd v,sd ;fs,d;q:;sxqs;q s;cds;, vqd;s,cqs, q ;c
+            |qsdqsdqsjb,;qns;dqddqs:n;,dg;dnfsqd
+            |qsdq
+            |qsdqqs,;snd;q,ds:;qsnd:bq,bd;,qbd;,qbdn;sqbdq
+            |dq:snd;qs:dnqs;d;:qsn,dbq;,dbq,;dq
+            |dqd:;sqnd,nqs:;snd;qsn;d,qnd,q,bfnqd;,alxeklfa:=sx;bgnfkaml=:fecklntuoijcmkxlgqchlekdjkr,lazjir xcgknfqxomcnq,ekjtln,fmnrljcn
+            |epf,mdqle;tk""".stripMargin,
+        ),
+      )
 
-  "ConcurrentConstantMemoryExcel#writeFile" should "not change the police size if the text is long" in {
-    val cms = newCMSPlz
+      addRows(cms, data0, 0)
 
-    val data0: Array[Row] = Array(
-      row(
-        """mqldnqs:;dn:q;nf;dqskdqshlfqldmqfnzlfnas:d,asqnf;q:dsd;nq:s;dnqs:;nd;qns:=:dkg;krgljz,q:snd:,;qs,:f:;qsfcsd v,sd ;fs,d;q:;sxqs;q s;cds;, vqd;s,cqs, q ;c
-          |qsdqsdqsjb,;qns;dqddqs:n;,dg;dnfsqd
-          |qsdq
-          |qsdqqs,;snd;q,ds:;qsnd:bq,bd;,qbd;,qbdn;sqbdq
-          |dq:snd;qs:dnqs;d;:qsn,dbq;,dbq,;dq
-          |dqd:;sqnd,nqs:;snd;qsn;d,qnd,q,bfnqd;,alxeklfa:=sx;bgnfkaml=:fecklntuoijcmkxlgqchlekdjkr,lazjir xcgknfqxomcnq,ekjtln,fmnrljcn
-          |epf,mdqle;tk""".stripMargin,
-      ),
-    )
+      val fileName = s"target/fileName-long-${new Date()}.xlsx"
 
-    addRows(cms, data0, 0)
+      writeFile(cms, fileName)
 
-    val fileName = s"target/fileName-long-${new Date()}.xlsx"
+      assertTrue(
+        new File(fileName).exists(),
+        cms.get().pages.nonEmpty,
+        !cms.get().pages.forall(page => Files.exists(page.path)), // clean the tmp CSV files automatically.
+      )
+    },
+    test("writeFile keeps the non ASCII characters") {
+      val cms = newCMSPlz
 
-    writeFile(cms, fileName)
+      val data0: Array[Row] = Array(
+        row("éàèç&ù$€£°"),
+      )
 
-    new File(fileName).exists() shouldBe true
-    cms.get().pages should not be empty
-    cms.get().pages.forall(page => Files.exists(page.path)) shouldBe false // clean the tmp CSV files automatically.
-  }
+      addRows(cms, data0, 0)
 
-  "ConcurrentConstantMemoryExcel#writeFile" should "keep the non ASCII character" in {
-    val cms = newCMSPlz
+      val fileName = s"target/fileName-non-ascii-${new Date()}.xlsx"
 
-    val data0: Array[Row] = Array(
-      row("éàèç&ù$€£°"),
-    )
+      writeFile(cms, fileName)
 
-    addRows(cms, data0, 0)
-
-    val fileName = s"target/fileName-non-ascii-${new Date()}.xlsx"
-
-    writeFile(cms, fileName)
-
-    new File(fileName).exists() shouldBe true
-    cms.get().pages should not be empty
-    cms.get().pages.forall(page => Files.exists(page.path)) shouldBe false // clean the tmp CSV files automatically.
-  }
+      assertTrue(
+        new File(fileName).exists(),
+        cms.get().pages.nonEmpty,
+        !cms.get().pages.forall(page => Files.exists(page.path)), // clean the tmp CSV files automatically.
+      )
+    },
+  )
 
 }
